0a1f138bf0a876835fbacec5246c10a30bbf9824
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-05-06 15:18:17 +0800

    intro talk addition

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace Chatbot
             //set the color of the input
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write(name + ": ");
-            return Console.ReadLine();
+            return Console.ReadLine().Trim().ToLower();
         }
 
         //introduction
@@ -46,9 +46,16 @@ namespace Chatbot
             {
                 String[] feedback25 = 
                 { 
-                    "Hey, I heard you've crossed the threshold of 25. So, now you've officially entered the realm of 'maturity and charm.' Congratulations!", 
-                    "I really admire you because you've survived that dreaded 25th birthday. How did you manage it? Did you invent the anti-aging elixir?", 
-                    "Age is just a number, but you seem to be making that number quite fashionable. Every time I see you, you look more charming and wise than before. Maybe I should seek your advice on how to stay forever young?" 
+                    "Hey, I heard you've crossed the threshold of 25.\n" +
+                    "\t So, now you've officially entered the realm of \n" + 
+                    "\t 'maturity and charm.' Congratulations!", 
+                    "I really admire you because you've survived that \n" + 
+                    "\t dreaded 25th birthday. How did you manage it? Did \n" + 
+                    "\t you invent the anti-aging elixir?", 
+                    "Age is just a number, but you seem to be making \n" + 
+                    "\t that number quite fashionable. Every time I see \n " +
+                    "\t you, you look more charming and wise than before. \n" +
+                    "\t Maybe I should seek your advice on how to stay forever young?" 
                 };
                 Random random = new Random();
                 output(feedback25[random.Next(0, feedback25.Length)]);
@@ -57,9 +64,15 @@ namespace Chatbot
             {
                 String[] feedback18 = 
                 { 
-                    "I heard you've surpassed the legal age of 18 and now you're a 'legally adventurous' individual. So, try some daring activities in life!", 
-                    "Now that you're in the world of adulthood, you still possess the energy and carefree attitude of a young person. That's amazing! Don't forget to share some secrets so us slightly older folks can keep up with the pace."
-                };
+                    "I heard you've surpassed the legal age of 18 and \n" +
+                    "\t now you're a 'legally adventurous' individual. \n" +
+                    "\t So, try some daring activities in life!", 
+                    "Now that you're in the world of adulthood, you \n " +
+                    "\t still possess the energy and carefree attitude \n " + 
+                    "\t of a young person. That's amazing! Don't forget \n" + 
+                    "\t to share some secrets so us slightly older folks \n" + 
+                    "\t can keep up with the pace."
+                }; 
                 Random random = new Random();
                 output(feedback18[random.Next(0, feedback18.Length)]);
             }
@@ -67,9 +80,18 @@ namespace Chatbot
             {
                 String[] feedback12 = 
                 { 
-                    "I heard you've joined the ranks of teenagers, gaining more freedom and responsibility. But I bet you still secretly wish to enjoy the delicious dinners your parents make for you, right?", 
-                    "Now that you've become a young adult, you have more autonomy and decision-making power. But don't forget to give yourself some time to be a kid as well, to enjoy playfulness and carefree moments.", 
-                    "You've reached that mysterious age where you're neither a child nor an adult. It's like an exploration journey, sometimes getting lost, sometimes discovering yourself. But no matter what, remember to enjoy this special time!" 
+                    "I heard you've joined the ranks of teenagers, gaining\n" + 
+                    "\t more freedom and responsibility. But I bet you \n" +
+                    "\t still secretly wish to enjoy the delicious dinners\n" +
+                    "\t your parents make for you, right?", 
+                    "Now that you've become a young adult, you have more \n" + 
+                    "\t autonomy and decision-making power. But don't forget\n" +
+                    "\t to give yourself some time to be a kid as well, to \n" +
+                    "\t enjoy playfulness and carefree moments.", 
+                    "You've reached that mysterious age where you're neither \n" +
+                    "\t a child nor an adult. It's like an exploration journey, \n" + 
+                    "\t sometimes getting lost, sometimes discovering yourself. \n" +
+                    "\t But no matter what, remember to enjoy this special time!" 
                 };
                 Random random = new Random();
                 output(feedback12[random.Next(0, feedback12.Length)]);
@@ -78,9 +100,13 @@ namespace Chatbot
             {
                 String[] feedback = 
                 { 
-                    " ", 
-                    " ", 
-                    " " 
+                    "Hey, young man/young lady, being around you makes me feel\n" +
+                    "\t like a kid again. Don't forget to enjoy every day of joy\n" + 
+                    "\t and carefree moments because sometimes, when you grow up,\n" +
+                    "\t you'll miss this stage!", 
+                    "You're a little genius! I heard you're a math whiz at school.\n" +
+                    "\t Next time I encounter a difficult problem, I'll come to \n" + 
+                    "\t you for help. "
                 };
                 Random random = new Random();
                 output(feedback[random.Next(0, feedback.Length)]);
